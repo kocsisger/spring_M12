d18c00665aaa006e553cd2f9fad0e21de9c3c037
Kocsis Gergely <kocsisger@example.com>
2025-11-03 12:54:38 +0100

    application properties updated

--- src/main/java/en/unideb/inf/SpringDataFX/SpringDataFxApplication.java
+++ src/main/java/en/unideb/inf/SpringDataFX/SpringDataFxApplication.java
@@ -1,13 +1,30 @@
 package en.unideb.inf.SpringDataFX;
 
+import en.unideb.inf.SpringDataFX.model.Person;
+import en.unideb.inf.SpringDataFX.model.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
-public class SpringDataFxApplication {
+public class SpringDataFxApplication implements CommandLineRunner {
+
+	@Autowired
+	PersonRepository personRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataFxApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Person p = new Person();
+		p.setName("John");
+		p.setDateOfBirth(LocalDate.of(2001,1,1));
+
+		personRepository.save(p);
+	}
 }
