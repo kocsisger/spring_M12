e8b1a3590b3c5c8efa50b1ad882cd9c2aa82648f
Kocsis Gergely <kocsisger@example.com>
2025-11-10 12:15:50 +0100

    Sping starts

--- src/main/java/en/unideb/inf/SpringDataFX/frontend/JavaFXApplication.java
+++ src/main/java/en/unideb/inf/SpringDataFX/frontend/JavaFXApplication.java
@@ -1,10 +1,12 @@
 package en.unideb.inf.SpringDataFX.frontend;
 
+import en.unideb.inf.SpringDataFX.SpringDataFxApplication;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
 
 public class JavaFXApplication extends Application {
     @Override
@@ -12,5 +14,6 @@ public class JavaFXApplication extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainPage.fxml"));
         stage.setScene(new Scene(root));
         stage.show();
+        SpringApplication.run(SpringDataFxApplication.class);
     }
 }
