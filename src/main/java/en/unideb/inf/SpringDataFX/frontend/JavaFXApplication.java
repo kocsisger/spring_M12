2ea33982e782fe6944f3952472e3d43c099e828d
Kocsis Gergely <kocsisger@example.com>
2025-11-10 12:21:20 +0100

    Spring start and close

--- src/main/java/en/unideb/inf/SpringDataFX/frontend/JavaFXApplication.java
+++ src/main/java/en/unideb/inf/SpringDataFX/frontend/JavaFXApplication.java
@@ -7,13 +7,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 public class JavaFXApplication extends Application {
+    ConfigurableApplicationContext ctx;
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainPage.fxml"));
         stage.setScene(new Scene(root));
         stage.show();
-        SpringApplication.run(SpringDataFxApplication.class);
+        ctx = SpringApplication.run(SpringDataFxApplication.class);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        ctx.close();
+        super.stop();
     }
 }
